da846eba607058b0fc1d8b0121c4157c739c71af
Dominique Skaf <doskaf@yahoo.com>
2019-09-15 19:49:39 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ def remove_strawberry(contacts)
     if person == "Freddy Mercury"
       data.each do |attribute, value|
         if attribute == :favorite_ice_cream_flavors
-          contacts.delete_if
+          contacts.delete_if {|attribute, value|
         end
       end
     end
